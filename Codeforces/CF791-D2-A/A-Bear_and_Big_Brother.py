a2b1712ca92f75aa55aa9ec8f1bc5ae0dda2287e
HaoES <hamza.essamaali@gmail.com>
2020-12-21 17:44:05 +0100

    added solution for Bear and Big Brother

---
+++ Codeforces/CF791-D2-A/A-Bear_and_Big_Brother.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import sys
+input = sys.stdin.readline
+
+############ ---- Input Functions, coutery of 'thekushalghosh' ---- ############
+
+
+def inp():
+    return(int(input()))
+
+
+def inlt():
+    return(list(map(int, input().split())))
+
+
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+
+
+def invr():
+    return(map(int, input().split()))
+
+
+if __name__ == "__main__":
+    a, b = invr()
+    y = 0
+    while a <= b:
+        a *= 3
+        b *= 2
+        y += 1
+    print(y)
---
+++ Codeforces/CF791-D2-A/README.md
@@ -0,0 +1,3 @@
+# Bear and Big Brother:
+
+This is the python solution for the problem [CF-791-D2-A: Bear and Big Brother](https://codeforces.com/contest/791/problem/A)
