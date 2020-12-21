8e46f8d35467eff81a892ec0f060e55f9d2fd66c
HaoES <hamza.essamaali@gmail.com>
2020-12-21 18:25:07 +0100

    added solution for A-Team problem

---
+++ Codeforces/CF231-D2-A/A-Team.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import sys
+input = sys.stdin.readline
+
+############ ---- Input Functions, coutery of 'thekushalghosh' ---- ############
+
+
+def inp():
+    return(int(input()))
+
+
+def inlt():
+    return(list(map(int, input().split())))
+
+
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+
+
+def invr():
+    return(map(int, input().split()))
+
+
+if __name__ == "__main__":
+    n = inp()
+    count = 0
+    for i in range(n):
+        l = sum(inlt())
+        if l > 1:
+            count += 1
+    print(count)
---
+++ Codeforces/CF231-D2-A/README.md
@@ -0,0 +1,3 @@
+# Team:
+
+This is the python solution for the problem [CF-231-D2-A: Team](https://codeforces.com/contest/231/problem/A).
