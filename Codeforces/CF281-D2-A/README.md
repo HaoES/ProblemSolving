1d5b8b968176148eef2a10844e905b8338c339d4
HaoES <hamza.essamaali@gmail.com>
2020-12-24 16:38:29 +0100

    added solution for Word Capitalization Problem

---
+++ Codeforces/CF281-D2-A/README.md
@@ -0,0 +1,2 @@
+# Word Capitalization
+This is the python solution for the problem [CF-281-D2-A: Word Capitalization](https://codeforces.com/contest/281/problem/A).
---
+++ Codeforces/CF281-D2-A/Word_Capitalization.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import sys
+input = sys.stdin.readline
+
+############ ---- Input Functions, coutery of 'thekushalghosh' ---- ############
+
+
+def inp():
+    return(int(input()))
+
+
+def inlt():
+    return(list(map(int, input().split())))
+
+
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+
+
+def invr():
+    return(map(int, input().split()))
+
+
+if __name__ == "__main__":
+    s = insr()
+    s[0] = s[0].upper()
+    print(''.join(s))
