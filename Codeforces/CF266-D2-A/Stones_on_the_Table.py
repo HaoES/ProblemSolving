27640f8fc03cf15af752128531a7b73d25e1c764
HaoES <hamza.essamaali@gmail.com>
2020-12-28 16:11:44 +0100

    added solution for the problem

---
+++ Codeforces/CF266-D2-A/Stones_on_the_Table.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import sys
+input = sys.stdin.readline
+
+############ ---- Input Functions, coutery of 'thekushalghosh' ---- ############
+
+
+def inp():
+    return(int(input()))
+
+
+def inlt():
+    return(list(map(int, input().split())))
+
+
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+
+
+def invr():
+    return(map(int, input().split()))
+
+
+if __name__ == "__main__":
+    n = int(input())
+    colors = insr()
+    count = 0
+    for i in range(n - 1):
+        if colors[i] == colors[i+1]:
+            count += 1
+    print(count)
