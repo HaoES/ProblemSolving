02c332cd3d272a580e39d2fdabad0dcf5aee4a64
HaoES <hamza.essamaali@gmail.com>
2021-01-07 18:37:48 +0100

    added a solution for the problem

---
+++ Codeforces/CF228-D2-A/Is_your_horseshoe_on_the_roof.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import sys
+input = sys.stdin.readline
+
+############ ---- Input Functions, coutery of 'thekushalghosh' ---- ############
+
+
+def inp():
+    return(int(input()))
+
+
+def inlt():
+    return(list(map(int, input().split())))
+
+
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+
+
+def invr():
+    return(map(int, input().split()))
+
+
+if __name__ == "__main__":
+    shoes = inlt()
+    unishoes = set(shoes)
+    print(len(shoes) - len(unishoes))
