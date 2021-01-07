5f7f21e8770e17a4aff8b2aeb0e11a037f721928
HaoES <hamza.essamaali@gmail.com>
2021-01-07 18:52:19 +0100

    added solution

---
+++ Codeforces/CF265-D2-A/Colorful_Stones.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import sys
+input = sys.stdin.readline
+
+############ ---- Input Functions, coutery of 'thekushalghosh' ---- ############
+
+
+def inp():
+    return(int(input()))
+
+
+def inlt():
+    return(list(map(int, input().split())))
+
+
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+
+
+def invr():
+    return(map(int, input().split()))
+
+
+if __name__ == "__main__":
+    stones = insr()
+    instructions = insr()
+    pointer = 0
+    for i in instructions:
+        if stones[pointer] == i:
+            pointer += 1
+    print(pointer + 1)
