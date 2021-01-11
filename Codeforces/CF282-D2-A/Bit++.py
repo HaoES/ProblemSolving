0083c4ac0429e0e674ea021e1718f54af49f3a4a
HaoES <hamza.essamaali@gmail.com>
2021-01-11 17:54:54 +0100

    added a solution

---
+++ Codeforces/CF282-D2-A/Bit++.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import sys
+input = sys.stdin.readline
+
+############ ---- Input Functions, coutery of 'thekushalghosh' ---- ############
+
+
+def inp():
+    return(int(input()))
+
+
+def inlt():
+    return(list(map(int, input().split())))
+
+
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+
+
+def invr():
+    return(map(int, input().split()))
+
+
+if __name__ == "__main__":
+    n = inp()
+    x = 0
+    for i in range(n):
+        instruction = insr()
+        if '+' in instruction:
+            x += 1
+        else:
+            x -= 1
+    print(x)
