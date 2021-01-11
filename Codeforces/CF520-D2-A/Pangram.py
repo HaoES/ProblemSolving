a7b64e249adf1a073bf7564824c7174b2e73acd0
HaoES <hamza.essamaali@gmail.com>
2021-01-11 18:22:17 +0100

    added a solution

---
+++ Codeforces/CF520-D2-A/Pangram.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import sys
+input = sys.stdin.readline
+
+############ ---- Input Functions, coutery of 'thekushalghosh' ---- ############
+
+
+def inp():
+    return(int(input()))
+
+
+def inlt():
+    return(list(map(int, input().split())))
+
+
+def insr():
+    s = input().lower()
+    return(list(s[:len(s) - 1]))
+
+
+def invr():
+    return(map(int, input().split()))
+
+
+if __name__ == "__main__":
+    n = inp()
+    string = insr()
+    if n < 26:
+        print('NO')
+    elif len(set(string)) == 26:
+        print('YES')
+    else:
+        print('NO')
