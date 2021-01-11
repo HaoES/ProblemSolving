10e7a9d7ec8c1adf0b71e623cb9e2bacdf9f87f7
HaoES <hamza.essamaali@gmail.com>
2021-01-11 12:51:49 +0100

    added a solution

---
+++ Codeforces/CF136-D2-A/Presents.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import sys
+input = sys.stdin.readline
+
+############ ---- Input Functions, coutery of 'thekushalghosh' ---- ############
+
+
+def inp():
+    return(int(input()))
+
+
+def inlt():
+    return(list(map(int, input().split())))
+
+
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+
+
+def invr():
+    return(map(int, input().split()))
+
+
+if __name__ == "__main__":
+    n = inp()
+    friends = inlt()
+    output = []
+    for i in range(n):
+        output.append(friends.index(i+1) + 1)
+    print(*output)
